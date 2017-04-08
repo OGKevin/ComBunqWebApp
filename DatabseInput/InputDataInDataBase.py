3e5b5bf9c3ff3639ee41a8d0935ba424550a1ca1
OGKevin <kevinhellemun@icloud.com>
2017-04-08 13:24:34 +0200

    getHeaders

--- DatabseInput/InputDataInDataBase.py
+++ DatabseInput/InputDataInDataBase.py
@@ -9,8 +9,20 @@ sys.path.insert(0, curdir)
 from Manager.models import catagories
 
 
-f = open('DatabseInput/ComBunqWeb-category-filter.csv')
-reader = csv.reader(f, delimiter=',', quotechar='"')
-keys = next(reader) #skip the headers
-out = [{key: val for key, val in zip(keys, prop)} for prop in reader]
-print json.dumps(out, sort_keys = True,indent=2)
+def getJSON():
+    catFiltersCSV = open('DatabseInput/ComBunqWeb-category-filter.csv')
+    reader = csv.reader(catFiltersCSV, delimiter=',', quotechar='"')
+    keys = next(reader)
+    catFiltersJSON = [{key:val for key,val in zip(keys,prop)} for prop in reader]
+    print json.dumps(catFiltersJSON,sort_keys=True,indent=2)
+    getHeaders()
+    
+
+def getHeaders():
+    catFiltersCSV = open('DatabseInput/ComBunqWeb-category-filter.csv')
+    reader = csv.reader(catFiltersCSV, delimiter=',', quotechar='"')
+    keys = reader.next()
+    print keys
+    
+
+getJSON()
