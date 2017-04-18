c3cb995cbc957b0af0d5201c3ebfc89ef27df06b
OGKevin <kevinhellemun@icloud.com>
2017-04-18 16:18:18 +0200

    debug mode false

--- BunqWebApp/settings.py
+++ BunqWebApp/settings.py
@@ -45,7 +45,7 @@ except NameError:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.combunqweb.herokuapp.com', '.127.0.0.1']
 
