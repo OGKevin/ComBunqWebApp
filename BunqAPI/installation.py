1459eb5647dc7cd19b4e102f98562e24fb9e1ab8
OGKevin <kevinhellemun@icloud.com>
2017-04-21 12:57:47 +0200

    Installation token gets saved
    Later need to make it that the user generates these info and the Token.json and Privatebunq.pem gets downlaoded so the user can store them. These files should then be deleted once the dowload action is complete.

--- BunqAPI/installation.py
+++ BunqAPI/installation.py
@@ -9,10 +9,13 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from pprint import pprint
-from bunq import API
+from pythonbunq.bunq import API
 import os
+import json
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# NOTE: generating private key and installation token
 
 
 def createKey():
@@ -30,7 +33,7 @@ def createKey():
       encryption_algorithm=serialization.NoEncryption()
     ).decode()
 
-    print(privateKey)
+    print(privateKey)  # NOTE: should not be printed in production
 
     # output PEM encoded version public part of private key
     publicKey = private_key.public_key().public_bytes(
@@ -38,7 +41,7 @@ def createKey():
       format=serialization.PublicFormat.SubjectPublicKeyInfo
     ).decode()
 
-    print(publicKey)
+    print(publicKey)  # NOTE: should not be printed in production
     p = open('privateBunq.pem', 'w')
     p.write(privateKey)
     p.close()
@@ -48,6 +51,8 @@ try:
     with open('privateBunq.pem', 'rb') as f:
         # IDEA: let users save and paste their generated private key when
         #  unsing the app?
+        # NOTE: this file should not be saved on the server
+        #  should be deleted once done w it
         rsa_key = f.read()
 
 except FileNotFoundError:  # noqa
@@ -69,4 +74,15 @@ pprint(dict(r.request.headers))
 print()
 pprint(dict(r.headers))
 print()
-pprint(r.json())  # NOTE: Need to save this information
+pprint(r.json())
+# NOTE: in production these shouldn't be printed
+
+response = r.json()
+
+with open('token.json', 'w') as f:
+    try:
+        # NOTE: this should aslo not be stored and deleted once done with
+        # the user should downlaod this file as well
+        json.dump(response['Response'][1]['Token'], f, indent=4)
+    except KeyError:
+        print (json.dumps(response, indent=4))
