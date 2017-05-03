41d71c7c597e482ed272187ab4e0d43287bf3311
OGKevin <kevinhellemun@icloud.com>
2017-05-03 19:22:25 +0200

    Butons are readey
    Need to make the view(API) to send the request to from JS
    
    Fixed the isse where u needed to press 'decrypt'(now 'register')
    before u could press 'start sessions'. --> #29
    
    However on the first press the respose code is still 500 due to the file
    not being read properly.

--- static/BunqAPI/JS/decrypt.js
+++ static/BunqAPI/JS/decrypt.js
@@ -1,5 +1,6 @@
 $(function() {
   var jsonObj;
+
   function get_file() {
     data = $("#id_encrypted_file")[0].files[0]
     var reader = new FileReader()
@@ -18,18 +19,41 @@ $(function() {
       "Too many requests. You can do a maximum of 1 POST call per 1 second to this endpoint."
   On the seccond click it work as intended with the exception of the above issue.
   */
-  $('#file_decrypt').click(function(event) {
+  $('#register').click(function(event) {
     /* Act on the event */
     // event.preventDefault();
     get_file()
     sendPost(jsonObj, 'register')
-    
-    $('#start_session').click(function(event) {
-      get_file()
-      // event.preventDefault();
-      sendPost(jsonObj, 'start_session')
-      /* Act on the event */
-    });
+  });
+
+  $('#start_session').click(function(event) {
+    get_file()
+    // event.preventDefault();
+    sendPost(jsonObj, 'start_session')
+  });
+  $("#users").click(function(event) {
+    /* Act on the event */
+    console.log('users');
+  });
+  $("#accounts").click(function(event) {
+    /* Act on the event */
+    console.log('accounts');
+  });
+  $("#set_up").click(function(event) {
+    /* Act on the event */
+    console.log('set_up');
+  });
+  $("#payment").click(function(event) {
+    /* Act on the event */
+    console.log('payment');
+  });
+  $("#card").click(function(event) {
+    /* Act on the event */
+    console.log('card');
+  });
+  $("#mastercard_action").click(function(event) {
+    /* Act on the event */
+    console.log('mastercard_action');
   });
 });
 
@@ -60,8 +84,9 @@ function sendPost(json, action) {
     .done(function(response) {
       r = JSON.parse(response)
       // $("#response").html(response)
-      if (r.Response){show(r.Response, false)}
-      else {
+      if (r.Response) {
+        show(r.Response, false)
+      } else {
         show(r, true)
       }
 
@@ -81,7 +106,7 @@ function show(j, error) {
     $("#response").html(j.error_description_translated)
 
   } else {
-  
+
     rendered = Mustache.render(template, j)
     $("#response").html(rendered)
   }
