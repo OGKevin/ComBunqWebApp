7bb725b3f3841d5f0de23bbf309eae908eafa7ca
OGKevin <kevinhellemun@icloud.com>
2017-05-17 22:53:09 +0200

    Cleaned JS

--- static/BunqAPI/JS/decrypt.js
+++ static/BunqAPI/JS/decrypt.js
@@ -1,4 +1,3 @@
-
 var dataTable;
 
 $(function() {
@@ -6,7 +5,6 @@ $(function() {
     userID = '',
     accountID = '';
   $("#lock_ids").click(function(event) {
-    /* Act on the event */
     userID = $('#userID').val()
     accountID = $("#accountID").val()
   });
@@ -21,58 +19,45 @@ $(function() {
     }
   }
   $("#load_file").click(function(event) {
-    /* Act on the event */
     get_file()
     $("#loading").html('File is loaded')
   });
   $('#register').click(function(event) {
-    /* Act on the event */
-    // event.preventDefault();
-    // get_file()
     sendPost(jsonObj, $(this)[0].id, register_template)
   });
 
   $('#start_session').click(function(event) {
-    // get_file()
-    // event.preventDefault();
     sendPost(jsonObj, $(this)[0].id, start_session_template)
   });
   $("#users").click(function(event) {
-    /* Act on the event */
     sendPost(jsonObj, $(this)[0].id + '/' + userID + '/', ussers_template)
   });
   $("#accounts").click(function(event) {
-    /* Act on the event */
     sendPost(jsonObj, $(this)[0].id + '/' + userID + '/' + accountID, accounts_template)
 
 
   });
   $("#payment").click(function(event) {
-    /* Act on the event */
     sendPost(jsonObj, $(this)[0].id + '/' + userID + '/' + accountID, payments_template)
-    
+
   });
   $("#card").click(function(event) {
-    /* Act on the event */
     sendPost(jsonObj, $(this)[0].id + '/' + userID + '/' + accountID, card_template)
-    
+
   });
-  $("#mastercard_action").click(function(event) {
-    /* Act on the event */
-  });
+  $("#mastercard_action").click(function(event) {});
   $("#export").click(function(event) {
-    /* Act on the event */
     pages = $("#pages").val()
     if (dataTable) {
       curentPage = dataTable.currentPage
       if (pages) {
-        
+
         dataTable.export('csv', 'Bunq-transactions', ';', '\r\n', [pages])
-      }else {
+      } else {
         dataTable.export('csv', 'Bunq-transactions', ';', '\r\n', curentPage)
-        
+
       }
-    }else {
+    } else {
       $("#loading").html('Table is not created yet?')
     }
   });
@@ -103,40 +88,30 @@ function sendPost(json, action, template) {
     })
     .done(function(response) {
       r = JSON.parse(response)
-      // r = response
-      // $("#response").html(response)
       if (r.Error !== undefined) {
-             show(r.Error[0], true)
-              $("#loading").html('')
-            }else if (r.Response[0].Payment) {
-              createTable(r.Response)
-              $("#loading").html('')
-            } else if (r.Response){
-              show(r.Response, false, template)
-              $("#loading").html('')
-            }
-      
-      //
-      // else {
-      //   show(r, true)
-      // }
-
-})
-    .fail(function() {})
-    .always(function() {});
+        show(r.Error[0], true)
+      } else if (r.Response[0].Payment) {
+        createTable(r.Response)
+      } else if (r.Response) {
+        show(r.Response, false, template)
+      }
+    })
+    .fail(function() {
+      e = {
+        'error_description_translated': 'Something went wrong server-side. Did you input you encrypted JSON file?'
+      }
+      show(e, true)
+    })
+    .always(function() {
+      $("#loading").html('')
+    });
 }
 
 function show(j, error, template) {
-  // var template = $('#template').html()
-  // // Mustache.parse(template)
-  // var rendered;
   if (error) {
     $("#response").html(j.error_description_translated)
 
   } else {
-
-    // rendered = Mustache.render(template, j)
-    // $("#response").html(rendered)
     $.get(template, function(template) {
       var rendered = Mustache.render(template, j)
       $('#response').html(rendered)
@@ -144,58 +119,42 @@ function show(j, error, template) {
   }
 }
 
-
-
 function createTable(input) {
-  
-    var rows = [],
-        headers = [
-          'Payment ID',
-          'Account ID',
-          'Date',
-          'Ammount',
-          'Account IBAN',
-          'Payee IBAN',
-          'Name',
-          'Description',
-          'Type'
-        ]
 
-    for (var i = 0; i < input.length; i++) {
-        rows.push([
-          input[i].Payment.id,
-          input[i].Payment.monetary_account_id,
-          input[i].Payment.updated,
-          input[i].Payment.amount.value,
-          input[i].Payment.alias.iban,
-          input[i].Payment.counterparty_alias.iban,
-          input[i].Payment.counterparty_alias.label_user.public_nick_name,
-          input[i].Payment.description,
-          input[i].Payment.type
-        ])
+  var rows = [],
+    headers = [
+      'Payment ID',
+      'Account ID',
+      'Date',
+      'Ammount',
+      'Account IBAN',
+      'Payee IBAN',
+      'Name',
+      'Description',
+      'Type'
+    ]
+
+  for (var i = 0; i < input.length; i++) {
+    rows.push([
+      input[i].Payment.id,
+      input[i].Payment.monetary_account_id,
+      input[i].Payment.updated,
+      input[i].Payment.amount.value,
+      input[i].Payment.alias.iban,
+      input[i].Payment.counterparty_alias.iban,
+      input[i].Payment.counterparty_alias.label_user.public_nick_name,
+      input[i].Payment.description,
+      input[i].Payment.type
+    ])
+  }
+  options = {
+    data: {
+      'headings': headers,
+      "rows": rows
     }
-    options = {
-        data: {
-            'headings': headers,
-            "rows": rows
-        }
-    }
-
-    // Check to see if initialized
-    if (dataTable) {
-        // Wipe the table
-        dataTable.destroy();
-
-        // Destroy the containers
-        // dataTable.wrapper.parentNode.replaceChild(dataTable.table, dataTable.wrapper);
-        // dataTable.init(options)
-    }
-
-    // Initialize with the data
-    dataTable = new DataTable("#response2", options);
-    // dataTable.destroy()
-}
-
-function exportTable(){
-  
+  }
+  if (dataTable) {
+    dataTable.destroy();
+  }
+  dataTable = new DataTable("#response2", options);
 }
